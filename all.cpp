5fb5e9c034a2b31be5832671d38104bf931f7f34
moksha515 <mokshabpatel@gmail.com>
2025-12-30 15:16:49 -0600

    added course name, course grade, credit info into arrays

--- all.cpp
+++ all.cpp
@@ -2,23 +2,19 @@
 This program collects user data like the students name, the courses they have taken the 
 past semester and the grade for each class, and calculates their semester GPA. 
 
-
-    in far, far future -- can put in university name && program can search up how many credits it takes and accounts
-    ( so user does not have to manually input this )
-
-
-    TMRW's agenda : 
-    - edit so that you can get access to each 
-      individual course name and course grade and can pull the value --> put all values into an array
+    TMRW's agenda :
     - organize user info into a table ( or adjacent ) */
   
 
 
-#include <bits/stdc++.h>
 #include <fstream>
 #include <iostream>
 using namespace std;
 
+const int COURSES_LENGTH = 10;
+const int CREDITS_LENGTH = 10;
+const int GRADES_LENGTH = 10;
+        
 int main()
 {
     //variables
@@ -29,15 +25,22 @@ int main()
     ofstream outputFile;
     
     int courseGrade = 0;
-    int numCourses = 0;
     int numCredits = 0;
     int totalCredits = 0;
+    int numCourses = 0;
     
     double sumQualityPts = 0.0;
     double gpa = 0.0;
     double qualityPts = 0.0;
     
     bool choice = true;
+
+
+
+    //arrays will have 0 in first value, then from 1, the actual values will be there 
+    string courses [COURSES_LENGTH];
+    int credits [CREDITS_LENGTH] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    int grades [GRADES_LENGTH] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
     
     
     
@@ -50,22 +53,30 @@ int main()
         cout << "Please enter your name : ";
         getline (cin, userName);
         outputFile << "Student Name : " << userName << "\n";
+
+        cout << "Please enter the number of courses : ";
+        cin >> numCourses;
+        outputFile << "Number of Courses : " << numCourses << "\n";
         
-        while (choice)
+        cin.ignore(1000, '\n');
+
+
+
+        int i = 0;
+        
+        while (numCourses > i)
         {
             cout << "\nPlease enter your Course Name ( or stop to finish ) : ";
             getline(cin, courseName);
-            if (courseName == "stop")
-            {
-                break;
-            }
+
             outputFile << "Course Name : " << courseName << "\n";
-            numCourses++;
             
             cout << "\nPlease enter how many credits the Course is : ";
             cin >> numCredits;
             outputFile << "Number of Credits : " << numCredits << "\n";
             totalCredits += numCredits;
+            
+
             
             
             cout << "\nPlease enter the grade you got in that course : ";
@@ -106,6 +117,14 @@ int main()
             
             // This clears the newline character so the next getline() works correctly
             cin.ignore(1000, '\n');
+
+
+            courses[i] = courseName;
+            credits[i] = numCredits;
+            grades[i] = courseGrade;
+            i++;
+
+
         }
         //calculating GPA 
         gpa = sumQualityPts / totalCredits; 
@@ -113,6 +132,36 @@ int main()
         cout << "\nTotal Credits : " << totalCredits;
         cout << "\nSemester GPA : " << gpa << endl;
         outputFile << "\nSemester GPA : " << gpa << endl;
+
+        cout << "Credits array : ";
+        // to print all of the users credit numbers 
+        for (int c = 0; c < CREDITS_LENGTH; c++)
+        {
+            if (credits[c] != 0)
+            {
+                cout << credits [c] << ", ";
+            }
+        }
+
+        cout << "Grades array : ";
+        // To print all of the users entered grades
+        for (int g = 0; g < GRADES_LENGTH; g++)
+        {
+            if (grades[g] != 0)
+            {
+                cout << grades [g] << ", ";
+            }
+        }
+        
+         cout << "Courses array : ";
+         // To print all of their courses names
+        for (int k = 0; k < COURSES_LENGTH; k++)
+        {
+            if (k < numCourses )
+            {
+                cout << courses[k] << " ";
+            }
+        }
         
         outputFile.close();
     }
